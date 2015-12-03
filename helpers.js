ba1bbf23e4ab17fcf4810b64c6681d76ff0eab95
mRoca <mroca.dh@gmail.com>
2015-12-03 01:55:32 +0100

    Add social data demo

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function random(start, end) {
     start = typeof start === 'number' ? start : null;
     end = typeof end === 'number' ? end : null;
@@ -15,4 +17,24 @@ function triangleCentroid(triangle) {
         (triangle[0][0] + triangle[1][0] + triangle[2][0]) / 3,
         (triangle[0][1] + triangle[1][1] + triangle[2][1]) / 3
     ];
+}
+
+function shuffle(array) {
+    var random = array.map(Math.random);
+    array.sort(function(a, b) {
+        return random[a] - random[b];
+    });
+
+    return array;
+}
+
+function deepClone(item) {
+    return JSON.parse(JSON.stringify(item));
+}
+
+function affine(point1, point2, x) {
+    var a = (point2[1] - point1[1]) / (point2[0] - point1[0]);
+    var b = (point2[0] * point1[1] - point1[0] * point2[1]) / (point2[0] - point1[0]);
+
+    return a * x + b;
 }
--- social-data.js
+++ social-data.js
@@ -5,18 +5,55 @@ var SocialData;
 (function() {
 
     var sources = [
-        'Facebook'
+        'Facebook',
+        'Twitter',
+        'Google search',
+        'Youtube',
+        'Linkedin'
     ];
 
-    SocialData = {
-        generateFakeData: function() {
-            var data = {};
-            for (var i = 0; i < sources.length; i++) {
-                data[sources[i]] = {};
+    var maxData = 50;
+    var wakeUpMinute = 4 * 60;
+    var sleepMinute = 27 * 50;
+    var clearMinute = sleepMinute + 3 * 50;
 
-                for (var j = 0; j < 30; j++) {
-                    data[sources[i]][j] = ~~random(100);
+    function generateData(x, previousData) {
+
+        var result = 0;
+
+        if (x > wakeUpMinute && x < sleepMinute) {
+            result = affine([wakeUpMinute, 0], [sleepMinute, maxData], x);
+
+            // Add some random data
+            result += random(-maxData / 10, maxData / 10);
+            result = Math.max(previousData || 0, result);
+        }
+
+        if (x >= sleepMinute) {
+            result = affine([sleepMinute, maxData], [clearMinute, 0], x);
+        }
+
+        return Math.max(0, result);
+    }
+
+    SocialData = {
+        generateFakeData: function(minutesIncrement) {
+
+            minutesIncrement = minutesIncrement || 1;
+
+            var data = {};
+            var previousData = {};
+            var hoursGeneration = 27;
+
+            for (var i = 0; i * minutesIncrement <= hoursGeneration * 60; i++) {
+                data[i] = {};
+
+                for (var j = 0; j < sources.length; j++) {
+                    var x = (i * minutesIncrement);
+                    data[i][sources[j]] = generateData(x, previousData[sources[j]]);
                 }
+
+                previousData = data[i];
             }
 
             return data;
